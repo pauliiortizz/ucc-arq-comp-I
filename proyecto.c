5320e6b4625c5f1680208d013585374d56149679
pauliiortizz <127853900+pauliiortizz@users.noreply.github.com>
2024-06-13 19:15:44 -0300

    Todas las secuencias en C

--- proyecto.c
+++ proyecto.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-//#include "EasyPIO.h"
+#include <termios.h>
 #include <stdint.h>
 #include <ncurses.h>
 #include <string.h>
@@ -30,22 +30,21 @@ char my_getch(void) {
 void delay (unsigned long int a) {
     unsigned int velocidad=a;
 
-            while(a>0){
-               if(my_getch()==KEY_DOWN){
-                   velocidad+=5000;
-                   a+=5000;
-               }
-               if((velocidad-5000)>=50000 && (a-5000)>=50000){
-                   if(my_getch()==KEY_UP){
-                       velocidad-=5000;
-                       a-=5000;
-                   }
-               }else{
-                   break;
-               }
-               a--;
+    while(a>0){
+        if(my_getch()==KEY_DOWN){
+            velocidad+=5000;
+            a+=5000;
+        }
+        if((velocidad-5000)>=50000 && (a-5000)>=50000){
+            if(my_getch()==KEY_UP){
+                velocidad-=5000;
+                a-=5000;
             }
-            //return velocidad;
+        }else{
+            break;
+        }
+        a--;
+    }
 }
 
 int kbhit(void) {
@@ -94,25 +93,27 @@ void ocultarEntrada(char* clave, int longitud) {
 void disp_binary(int i){
     int t;
     for(t=128;t>0;t=t/2)
-        if(i&t) printf("*");
-        else printf("_");
-        printf("\n");
+        if(i&t) printw("*");
+        else printw("_");
+    printw("\n");
 }
 
 void AutoFantastico(void){
-    char output;
-    char t, j=1;
+    unsigned int output;
+    int t, j=1;
     int on_time=1;
 
     initscr(); // Iniciar ncurses
     noecho(); // No mostrar teclas presionadas
     keypad(stdscr, TRUE); // Habilitar teclas especiales
 
-    printf("Secuencia en accion ... Oprima 'e' para finalizar\n");
+    printw("Secuencia en accion ... Oprima 'e' para finalizar\n");
+    refresh();
     do{
         output=0x80;
         for(t=0; t<8; t++){
             disp_binary(output);
+            refresh();
             sleep(on_time);
             output=output>>1;
         }
@@ -120,6 +121,7 @@ void AutoFantastico(void){
         for(t=0;t<6;t++){
             output=output<<1;
             disp_binary(output);
+            refresh();
             sleep(on_time);
         }
 
@@ -133,18 +135,98 @@ void AutoFantastico(void){
     }while(1); //repite hasta que se toca una tecla
     output=0x00;
     disp_binary(output);
-    printf("Chau...\n");
+    printw("Chau...\n");
+    refresh();
     endwin(); // Finalizar ncurses
 }
 
-void Choque(void) {
-    printf("Mostranco Choque: \n");
-    char a;
+void choque(void) {
+    initscr(); // Iniciar ncurses
+    noecho(); // No mostrar teclas presionadas
+    keypad(stdscr, TRUE); // Habilitar teclas especiales
+
+    printw("Mostrando Choque: \n");
     unsigned char tabla[7] = {0x81, 0x42, 0x24, 0x18, 0x18, 0x24, 0x42};
     for (int i = 0; i < 7; i++) {
         disp_binary(tabla[i]);
-        sleep(1000); // Convertir a milisegundos
+        refresh();
+        usleep(1000 * 1000); // Convertir a microsegundos
     }
+
+    printw("Chau...\n");
+    refresh();
+    endwin(); // Finalizar ncurses
+}
+
+void parpadeoAlternado(void) {
+    unsigned int output;
+    int on_time = 1;
+
+    initscr(); // Iniciar ncurses
+    noecho(); // No mostrar teclas presionadas
+    keypad(stdscr, TRUE); // Habilitar teclas especiales
+
+    printw("Secuencia de parpadeo alternado ... Oprima 'e' para finalizar\n");
+    refresh();
+    
+    do {
+        // Mostrar posiciones impares
+        output = 0xAA; // 10101010 en binario, representa las posiciones impares
+        disp_binary(output);
+        refresh();
+        sleep(on_time);
+        
+        // Mostrar posiciones pares
+        output = 0x55; // 01010101 en binario, representa las posiciones pares
+        disp_binary(output);
+        refresh();
+        sleep(on_time);
+        
+        if (kbhit()) {
+            char c = my_getch();
+            if (c == 'e') {
+                break;
+            }
+        }
+        
+    } while (1); // Repite hasta que se toque una tecla
+    
+    output = 0x00;
+    disp_binary(output);
+    printw("Chau...\n");
+    refresh();
+    endwin(); // Finalizar ncurses
+}
+
+void olaOceanica(void) {
+    initscr(); // Iniciar ncurses
+    noecho(); // No mostrar teclas presionadas
+    keypad(stdscr, TRUE); // Habilitar teclas especiales
+
+    printw("Mostrando Ola Oceánica: \n");
+
+    unsigned char tabla[] = {
+        0x80, 0xC0, 0xE0, 0xF0, 0xF8, 0xFC, 0xFE, 0xFF, // Ola moviéndose de izquierda a derecha
+        0x7F, 0x3F, 0x1F, 0x0F, 0x07, 0x03, 0x01, 0x00  // Ola moviéndose de derecha a izquierda
+    };
+
+    do {
+        for (int i = 0; i < sizeof(tabla); i++) {
+            disp_binary(tabla[i]);
+            refresh();
+            usleep(500000); // 0.5 segundos
+
+            if (kbhit()) {
+                char c = my_getch();
+                if (c == 'e') {
+                    endwin();
+                    return;
+                }
+            }
+        }
+    } while (1); // Repite hasta que se toque una tecla
+
+    endwin(); // Finalizar ncurses
 }
 
 void mostrarMenu() {
@@ -153,7 +235,7 @@ void mostrarMenu() {
     printf("\t1. El Auto Fantastico\n");
     printf("\t2. El Choque\n");
     printf("\t3. Parpadeo Alternado\n");
-    printf("\t4. Ola oceánica\n");
+    printf("\t4. Ola oceÃ¡nica\n");
     printf("\t5. Salir\n");
 }
 
@@ -162,7 +244,7 @@ int main() {
     int intento = 0;
     int ingresa = 0;
 
-    // Contraseña única guardada en memoria
+    // ContraseÃ±a Ãºnica guardada en memoria
     char claveGuardada[] = "12345";
 
     do {
@@ -193,432 +275,10 @@ int main() {
                     AutoFantastico();
                     break;
                 case 2:
-                    Choque();
-                    break;
-                case 3:
-                   //parpadeoAlternado();
-                    break;
-                case 4:
-                    //olaOceanica();
-                    break;
-                case 5:
-                    printf("\n\tSaliendo...\n");
-                    break;
-                default:
-                    printf("\n\tOpcion no valida\n");
-            }
-
-            getchar(); // Limpiar el buffer de entrada
-            printf("\n\n\tPresione Enter para continuar...");
-            getchar(); // Espera a que el usuario presione Enter
-
-            if (opcion == 5) // Salir del bucle si el usuario selecciona la opción de salir
-                break;
-
-            system(CLEAR); // Limpiar la pantalla antes de mostrar el menú nuevamente
-        } while (1);
-    } else {
-        printf("\n\n\tHa sobrepasado el número máximo de intentos permitidos\n");
-    }
-
-    return 0;
-}
-
-/* #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
-#ifdef _WIN32
-#include <conio.h>
-#include <windows.h>
-#define CLEAR "cls"
-void delay(unsigned long int milliseconds) {
-    Sleep(milliseconds);
-}
-#else
-#include <termios.h>
-#include <unistd.h>
-#include <sys/select.h>
-#define CLEAR "clear"
-void delay(unsigned long int microseconds) {
-    usleep(microseconds);
-}
-#endif
-
-#define TECLA_ENTER 13
-#define TECLA_BACKSPACE 8
-#define LONGITUD_CLAVE 5
-#define MAX_INTENTOS 3
-#define SPEED_INCREMENT 100000
-
-int speed = 500000; // 500ms
-
-#ifndef _WIN32
-int kbhit(void) {
-    struct timeval tv = { 0L, 0L };
-    fd_set fds;
-    FD_ZERO(&fds);
-    FD_SET(0, &fds);
-    return select(1, &fds, NULL, NULL, &tv) > 0;
-}
-
-char getch(void) {
-    char c;
-    struct termios oldt, newt;
-    tcgetattr(STDIN_FILENO, &oldt);
-    newt = oldt;
-    newt.c_lflag &= ~(ICANON | ECHO);
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-    c = getchar();
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-    return c;
-}
-#endif
-
-void ocultarEntrada(char* clave, int longitud) {
-    int i = 0;
-    char caracter;
-
-#ifdef _WIN32
-    while (1) {
-        caracter = getch();
-
-        if (caracter == TECLA_ENTER) {
-            clave[i] = '\0';
-            break;
-        }
-        else if (caracter == TECLA_BACKSPACE) {
-            if (i > 0) {
-                i--;
-                printf("\b \b");
-            }
-        }
-        else {
-            if (i < longitud) {
-                printf("*");
-                clave[i] = caracter;
-                i++;
-            }
-        }
-    }
-#else
-    struct termios oldt, newt;
-    tcgetattr(STDIN_FILENO, &oldt);
-    newt = oldt;
-    newt.c_lflag &= ~(ECHO | ICANON);
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-
-    while (1) {
-        caracter = getchar();
-
-        if (caracter == '\n' || caracter == '\r') {
-            clave[i] = '\0';
-            break;
-        }
-        else if (caracter == 127 || caracter == TECLA_BACKSPACE) {
-            if (i > 0) {
-                i--;
-                printf("\b \b");
-                fflush(stdout);
-            }
-        }
-        else {
-            if (i < longitud) {
-                printf("*");
-                fflush(stdout);
-                clave[i] = caracter;
-                i++;
-            }
-        }
-    }
-
-    newt.c_lflag |= (ECHO | ICANON);
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-#endif
-}
-
-void autoFantastico() {
-    int leds[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08 };
-    int num_leds = sizeof(leds) / sizeof(leds[0]);
-
-    printf("Auto fantástico en ejecución. Presiona 'q' para salir.\n");
-    while (1) {
-        for (int i = 0; i < num_leds; i++) {
-            for (int j = 0; j < num_leds; j++) {
-                if (j == i) {
-                    printf("* ");
-                } else {
-                    printf("- ");
-                }
-            }
-            printf("\n");
-            fflush(stdout);
-#ifdef _WIN32
-            delay(speed / 1000);  // Convert microseconds to milliseconds for Windows
-#else
-            delay(speed);
-#endif
-
-            if (kbhit()) {
-                char c = getch();
-                if (c == 'q') {
-                    printf("\nSaliendo de Auto fantástico...\n");
-                    return;
-                } else if (c == 'u' && speed > SPEED_INCREMENT) {
-                    speed -= SPEED_INCREMENT;
-                } else if (c == 'd') {
-                    speed += SPEED_INCREMENT;
-                }
-            }
-        }
-        for (int i = num_leds - 2; i > 0; i--) {
-            for (int j = 0; j < num_leds; j++) {
-                if (j == i) {
-                    printf("* ");
-                } else {
-                    printf("- ");
-                }
-            }
-            printf("\n");
-            fflush(stdout);
-#ifdef _WIN32
-            delay(speed / 1000);  // Convert microseconds to milliseconds for Windows
-#else
-            delay(speed);
-#endif
-
-            if (kbhit()) {
-                char c = getch();
-                if (c == 'q') {
-                    printf("\nSaliendo de Auto fantástico...\n");
-                    return;
-                } else if (c == 'u' && speed > SPEED_INCREMENT) {
-                    speed -= SPEED_INCREMENT;
-                } else if (c == 'd') {
-                    speed += SPEED_INCREMENT;
-                }
-            }
-        }
-    }
-}
-
-void choque() {
-    const char *tabla_choque[] = {
-            "* - - - - - - *",
-            " * - - - - - * ",
-            "- * - - - - * -",
-            "- -* - - - *- -",
-            "- - * - - *- - ",
-            "- - -* - *- - -",
-            "- - - * *- - - ",
-            "- - - -* - - - ",
-            "- - - * *- - - ",
-            "- - -* - *- - -",
-            "- -* - - * - -",
-            "- * - - - *- -",
-            " * - - - - * -",
-            "* - - - - - * ",
-            "* - - - - - - *"
-    };
-
-    int num_secuencias = sizeof(tabla_choque) / sizeof(tabla_choque[0]);
-
-    printf("Secuencia de Choque. Presiona 'q' para salir.\n");
-
-    while (1) {
-        for (int i = 0; i < num_secuencias; i++) {
-            printf("%s\n", tabla_choque[i]);
-            fflush(stdout);
-#ifdef _WIN32
-            delay(speed / 1000);
-#else
-            delay(speed);
-#endif
-
-            if (kbhit()) {
-                char c = getch();
-                if (c == 'q') {
-                    printf("Saliendo de Secuencia de Choque...\n");
-                    return;
-                } else if (c == 'u' && speed > SPEED_INCREMENT) {
-                    speed -= SPEED_INCREMENT;
-                } else if (c == 'd') {
-                    speed += SPEED_INCREMENT;
-                }
-            }
-        }
-    }
-}
-
-
-void parpadeoAlternado() {
-    int leds[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08 };
-    int num_leds = sizeof(leds) / sizeof(leds[0]);
-
-    printf("Parpadeo alternado en ejecución. Presiona 'q' para salir.\n");
-    while (1) {
-        // Patrón 1: LED 1, 3, 5, 7 encendidos; LED 2, 4, 6, 8 apagados
-        for (int i = 0; i < num_leds; i++) {
-            if (i % 2 == 0) {
-                printf("* ");
-            } else {
-                printf("- ");
-            }
-        }
-        printf("\n");
-        fflush(stdout);
-#ifdef _WIN32
-        delay(speed / 1000);  // Convert microseconds to milliseconds for Windows
-#else
-        delay(speed);
-#endif
-
-        if (kbhit()) {
-            char c = getch();
-            if (c == 'q') {
-                printf("\nSaliendo de Parpadeo alternado...\n");
-                return;
-            } else if (c == 'u' && speed > SPEED_INCREMENT) {
-                speed -= SPEED_INCREMENT;
-            } else if (c == 'd') {
-                speed += SPEED_INCREMENT;
-            }
-        }
-
-        // Patrón 2: LED 2, 4, 6, 8 encendidos; LED 1, 3, 5, 7 apagados
-        for (int i = 0; i < num_leds; i++) {
-            if (i % 2 == 1) {
-                printf("* ");
-            } else {
-                printf("- ");
-            }
-        }
-        printf("\n");
-        fflush(stdout);
-#ifdef _WIN32
-        delay(speed / 1000);  // Convert microseconds to milliseconds for Windows
-#else
-        delay(speed);
-#endif
-
-        if (kbhit()) {
-            char c = getch();
-            if (c == 'q') {
-                printf("\nSaliendo de Parpadeo alternado...\n");
-                return;
-            } else if (c == 'u' && speed > SPEED_INCREMENT) {
-                speed -= SPEED_INCREMENT;
-            } else if (c == 'd') {
-                speed += SPEED_INCREMENT;
-            }
-        }
-    }
-}
-
-void olaOceanica() {
-    // Tabla de datos para la secuencia de ola
-    const char *tabla_secuencia[] = {
-        "* - - - - - - -",
-        "* * - - - - - -",
-        "* * * - - - - -",
-        "* * * * - - - -",
-        "* * * * * - - -",
-        "* * * * * * - -",
-        "* * * * * * * -",
-        "* * * * * * * *",
-        "- * * * * * * *",
-        "- - * * * * * *",
-        "- - - * * * * *",
-        "- - - - * * * *",
-        "- - - - - * * *",
-        "- - - - - - * *",
-        "- - - - - - - *",
-        "- - - - - - - -"
-    };
-
-    // Variables locales
-    int num_secuencias = sizeof(tabla_secuencia) / sizeof(tabla_secuencia[0]);
-    int speed = 500000; // 0.5 segundos
-
-    printf("Secuencia de Ola. Presiona 'q' para salir.\n");
-
-    while (1) {
-        for (int i = 0; i < num_secuencias; i++) {
-            printf("%s\n", tabla_secuencia[i]);
-            fflush(stdout);
-            //usleep(speed);  para MacOS
-           // _sleep(speed); // para Windows
-#ifdef _WIN32
-            delay(speed / 1000);
-#else
-            delay(speed);
-#endif
-
-            if (kbhit()) {
-                char c = getch();
-                if (c == 'q') {
-                    printf("Saliendo de Secuencia de Ola...\n");
-                    return;
-                } else if (c == 'u' && speed > SPEED_INCREMENT) {
-                    speed -= SPEED_INCREMENT;
-                } else if (c == 'd') {
-                    speed += SPEED_INCREMENT;
-                }
-            }
-        }
-    }
-}
-
-void mostrarMenu() {
-    printf("\n\n\tMENU\n");
-    printf("\t----\n");
-    printf("\t1. El Auto Fantastico\n");
-    printf("\t2. El Choque\n");
-    printf("\t3. Parpadeo Alternado\n");
-    printf("\t4. Ola oceánica\n");
-    printf("\t5. Salir\n");
-}
-
-int main() {
-    char clave[LONGITUD_CLAVE + 1];
-    int intento = 0;
-    int ingresa = 0;
-
-    // Contraseña única guardada en memoria
-    char claveGuardada[] = "12345";
-
-    do {
-        system(CLEAR);
-        printf("\n\tCLAVE: ");
-        ocultarEntrada(clave, LONGITUD_CLAVE);
-
-        if (strcmp(clave, claveGuardada) == 0) {
-            ingresa = 1;
-        } else {
-            printf("\n\n\tClave incorrecta\n");
-            intento++;
-            getchar(); // Espera a que el usuario presione Enter
-        }
-    } while (intento < MAX_INTENTOS && ingresa == 0);
-
-    if (ingresa == 1) {
-        printf("\n\n\tBienvenido al Sistema\n");
-        int opcion;
-
-        do {
-            mostrarMenu();
-            printf("\n\tSeleccione una opcion: ");
-            scanf("%d", &opcion);
-
-            switch (opcion) {
-                case 1:
-                    autoFantastico();
-                    break;
-                case 2:
                     choque();
                     break;
                 case 3:
-                    parpadeoAlternado();
+                   parpadeoAlternado();
                     break;
                 case 4:
                     olaOceanica();
@@ -634,15 +294,14 @@ int main() {
             printf("\n\n\tPresione Enter para continuar...");
             getchar(); // Espera a que el usuario presione Enter
 
-            if (opcion == 5) // Salir del bucle si el usuario selecciona la opción de salir
+            if (opcion == 5) // Salir del bucle si el usuario selecciona la opciÃ³n de salir
                 break;
 
-            system(CLEAR); // Limpiar la pantalla antes de mostrar el menú nuevamente
+            system(CLEAR); // Limpiar la pantalla antes de mostrar el menÃº nuevamente
         } while (1);
     } else {
-        printf("\n\n\tHa sobrepasado el número máximo de intentos permitidos\n");
+        printf("\n\n\tHa sobrepasado el nÃºmero mÃ¡ximo de intentos permitidos\n");
     }
 
     return 0;
 }
-*/
