6389b0e48e108c1da356718047eb0b5dd586e052
pauliiortizz <127853900+pauliiortizz@users.noreply.github.com>
2024-06-17 14:57:08 -0300

    Add files via upload

---
+++ assembly_functions.h
@@ -0,0 +1,9 @@
+//
+// Created by Maximo Chattas on 08/06/2023.
+//
+
+#ifndef PROYECTO_ASSEMBLY_FUNCTIONS_H
+#define PROYECTO_ASSEMBLY_FUNCTIONS_H
+extern void sirena(void);
+extern void shiftLights(void);
+#endif //PROYECTO_ASSEMBLY_FUNCTIONS_H
