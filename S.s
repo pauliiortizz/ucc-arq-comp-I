08b12675009a43b3f0810d1ad851c1bb8e0ce1da
pauliiortizz <paulinaortizilr5@gmail.com>
2024-06-24 19:40:45 -0300

    .

--- S.s
+++ S.s
@@ -28,54 +28,54 @@ FINAL:
 
 
 .global olaOceanica
-olaOceanica: 
-
+olaOceanica:
       PUSH {R4, R5, R6, R7, LR}
       
-start: MOV R5, #0x01        // Iniciar con un LED encendido
-      MOV R7, #8           // NÃºmero de LEDs (o pasos) hacia un lado
-      MOV R4, #255
-      ADD R4, R4, R4
+start: MOV R5, #0x01  ; Inicializa R5 con el bit más bajo
+      MOV R7, #7      ; Inicializa R7 con el contador para 7 iteraciones
+      MOV R4, #255    ; Inicializa R4 con 255
+      ADD R4, R4, R4  ; Duplica R4, ahora es 510
+      MOV R3, #0      ; Inicializa R3 como acumulador de los 1s
 
-// Movimiento de ida
 loop1: MOV R6, R5
-      ORR R5, R5, R6       // Conservar los LEDs encendidos previamente
-      MOV R0, R5
-      MOV R1, #5
-      BL disp_binaryAssembly
+      LSL R5, R5, #1  ; Desplazamiento lógico a la izquierda
+      ORR R3, R3, R6  ; Acumula los 1s en R3
+      MOV R0, R3
+      BL disp_binary  ; Muestra el valor en binario
       MOV R0, R4
-      BL delayAssembly
+      BL delayAssembly ; Retraso
       CMP R0, #0
       BEQ TERMINO
       MOV R4, R0
 
-      SUBS R7, R7, #1
+      SUBS R7,R7,#1
       BNE loop1
 
-      MOV R7, #8           // Reiniciar el contador de pasos
-      MOV R5, #0xFF        // Iniciar con todos los LEDs encendidos para la vuelta
+      MOV R7, #7
+      MOV R5, #0x80  ; Inicializa R5 con el bit más alto
 
-// Movimiento de regreso
 loop2: MOV R6, R5
-      BIC R5, R5, R6       // Apagar LEDs uno por uno
-      MOV R0, R5
-      MOV R1, #5
-      BL disp_binaryAssembly
+      LSR R5, R5, #1  ; Desplazamiento lógico a la derecha
+      ORR R3, R3, R6  ; Acumula los 1s en R3
+      MOV R0, R3
+      MOV R1,#5
+      BL disp_binaryAssembly ; Muestra el valor en binario
       MOV R0, R4
-      BL delayAssembly
+      BL delayAssembly ; Retraso
       CMP R0, #0
       BEQ TERMINO
       MOV R4, R0
 
-      SUBS R7, R7, #1
+      SUBS R7,R7,#1
       BNE loop2
 
-      BL delayAssembly
-      CMP R0, #0
-      BNE start
+      MOV R7, #7
+      MOV R5, #0x01  ; Reinicia el ciclo con el bit más bajo
 
- TERMINO: POP {R4, R5, R6, R7, PC}
- 
+      B start        ; Vuelve al inicio para crear un bucle infinito
+
+TERMINO: 
+      POP {R4, R5, R6, R7, PC}
 
 .data
 array:
